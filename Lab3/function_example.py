080b5072f975274d52e4b452169de3d0fc1c8ca0
phuongd2-lgtm <phuongd2@hawaii.edu>
2026-01-27 12:31:47 -1000

    Create function_example.py

---
+++ Lab3/function_example.py
@@ -0,0 +1,7 @@
+# An example of creating and user of functions in Python
+# Name: Phuong Duong
+# Date: Jan 22, 2026
+
+def greet(name):
+    """This function greets the person whose name is possed in ."""
+    print(f"Hello, {name}! Welcome to the program!")
