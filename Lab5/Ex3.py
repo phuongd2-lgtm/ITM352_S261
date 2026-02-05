6a355e882d4be306ddbb63e77e30f5172db9dd76
phuongd2-lgtm <phuongd2@hawaii.edu>
2026-02-05 13:31:10 -1000

    Create Ex3.py

---
+++ Lab5/Ex3.py
@@ -0,0 +1,7 @@
+reposnses = [5,7,3,8]
+respondent_ids = (1012, 1035, 1021, 1053)
+
+survey_dict = dict(zip(respondent_ids, responses))
+print("Survey responses with respondent IDs:", survey_dict)
+
+print(f"respndent (respondent_ids[0]) gave a response of {survey_dict[respondent_ids[0]]}")
