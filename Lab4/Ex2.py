13f1f5d68a64b818dac866b0141c8d1c5b7829de
phuongd2-lgtm <phuongd2@hawaii.edu>
2026-02-05 13:30:52 -1000

    Create Ex2.py

---
+++ Lab4/Ex2.py
@@ -0,0 +1,17 @@
+# Name: Phuong Duong
+# Date: Jan 30, 2026
+
+responses = [5, 7, 3, 8]
+respondent_ids = (1012, 1035, 1021, 1053)
+
+responses.append(respondent_ids)
+print(responses)
+# The output will be: [5, 7, 3, 8, (1012, 1035, 1021, 1053)]
+
+# list of tupes 
+data = [(1012, 5), (1035, 7), (1021, 3), (1053, 8)]
+print(data)
+
+#Dictionary
+data = {1012: 5, 1035: 7, 1021: 3, 1053: 8}
+print(data)
